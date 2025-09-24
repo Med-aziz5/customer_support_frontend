085198b59337aa8f4a56d95f589e35e9ea809430
aziz mathlouthi <aziz.mathlouthi66@gmail.com>
2025-09-24 19:51:17 +0100

    jwt iterceptor fixed

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
@@ -11,6 +12,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
     provideRouter(routes),
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
-  ]
+    provideHttpClient(withInterceptors([JwtInterceptor])),
+  ],
 }).catch(err => console.error(err));

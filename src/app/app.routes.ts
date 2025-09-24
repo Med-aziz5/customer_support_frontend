822d9968cec378e3e6972e42dd7567c446f7ef00
aziz mathlouthi <aziz.mathlouthi66@gmail.com>
2025-09-24 20:29:41 +0100

    guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { TicketsComponent } from './tickets/tickets.component'; // placeholder for after-login view
+import { AuthGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'tickets', component: TicketsComponent }, // protected route later
+  { path: 'tickets', component: TicketsComponent,canActivate:[AuthGuard] }, 
   { path: '**', redirectTo: 'login' }, // fallback
 ];
